ecfd46142e716485b2aebfbb77f3dae761ea10fe
Dedenmhmmd7 <126851684+Dedenmhmmd7@users.noreply.github.com>
2023-04-03 12:48:09 +0700

    Add files via upload

---
+++ latihan3/overload1.py
@@ -0,0 +1,7 @@
+#Nama : ALI MABRUR MUBAROK
+#Nim : 210511112
+#Kelas : TIF21C / R3
+
+print(min(2, 5)) 
+print(min([1, 2, 3])) 
+print(min("football"))
---
+++ latihan3/overload2.py
@@ -0,0 +1,7 @@
+#Nama : ALI MABRUR MUBAROK
+#Nim : 210511112
+#Kelas : TIF21C / R3
+
+b = ["c", 2, "a", 6, "b", "c", 1, 5, 4,"d", 3, "e"] 
+b.remove(5)
+print(b)
